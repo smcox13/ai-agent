9ea793bd998cf37f0d8c72051c679b752fef9d49
smcox13 <thestevencox@gmail.com>
2025-10-20 11:55:21 -0400

    CH2 start

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
-import os
 import sys
-from dotenv import load_dotenv
+import os
+import argparse
 from google import genai
-
+from google.genai import types
+from dotenv import load_dotenv
 
 
 def main():
-    print("Hello from ai-agent!")
-
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    try: 
-        response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents=sys.argv[1] 
-            )
+    parser = argparse.ArgumentParser()
+    parser.add_argument("user_prompt")                    
+    parser.add_argument("--verbose", action="store_true")  # boolean flag
+    args = parser.parse_args()
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)]),
+    ]
+
+    generate_content(client, messages, args)
+
+
+def generate_content(client, messages, args):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    if args.verbose:
+        print("User prompt:", args.user_prompt)
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     
-    except:
-        print("Error: No prompt provided")
-        sys.exit(1)
-    
+    print("Response:")
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
